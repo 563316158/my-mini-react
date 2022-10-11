7fbc6c22fac8e90a71e261d765945590989a4b61
Jerry.Qin 秦利杰 <jerry.qin@youniverse.cc>
2022-10-11 09:42:28 +0800

    完成creatFiber

---
+++ src/ReactWorkLoop.js
@@ -0,0 +1,58 @@
+import {
+  updateClassComponent,
+  updateFragmentComponent,
+  updateFunctionComponent,
+  updateHostComponent,
+  updateHostTextComponent,
+} from "./ReactReconciler";
+import {
+  ClassComponent,
+  FunctionComponent,
+  HostComponent,
+  HostText,
+} from "./ReactWorkTags";
+
+let wip = null; // work in progress
+
+function performUnitOfWork() {
+  const { tag } = wip;
+
+  // tode 1.更新当前组件
+  switch (tag) {
+    case HostComponent:
+      updateHostComponent(wip);
+      break;
+    case FunctionComponent:
+      updateFunctionComponent(wip);
+      break;
+    case ClassComponent:
+      updateClassComponent(wip);
+      break;
+    case Fragment:
+      updateFragmentComponent(wip);
+      break;
+    case HostText:
+      updateHostTextComponent(wip);
+      break;
+    default:
+      break;
+  }
+
+  // todo 2.下一个更新谁 深度优先遍历（国王的故事）
+  if (wip.child) {
+    wip = wip.child;
+    return;
+  }
+
+  let next = wip;
+
+  while (next) {
+    if (next.sibling) {
+      wip = next.sibling;
+      return;
+    }
+    next = next.return;
+  }
+
+  wip = null;
+}
